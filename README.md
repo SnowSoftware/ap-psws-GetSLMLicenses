627c4edefd551cb7d0466ecc93f6e74e890b2cdb
mrorre <oskar.frolin@snowsoftware.com>
2024-01-25 23:59:43 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Template-Public-OpenSource-Repo
-template repo for projects to be published publicly and shared with customers outside of snow
+# ap-psws-GetSLMLicenses
+
+## Description
+Snow Automation Platform PowerShell WebService that retrieves licenses from Snow License Manager. Will return license(s) either by filter (See SLM API documentation for filter instructions) if supplied, or single license by Id if supplied, or else all licenses. 
+
+## Object properties
+Licenses returned will have the following properties:
+  
+* Id              
+* ApplicationName 
+* ManufacturerName
+* Metric          
+* AssignmentType  
+* PurchaseDate    
+* Quantity        
+* IsIncomplete    
+* UpdatedDate     
+* UpdatedBy       
+
+```json
+['Id','ApplicationName','ManufacturerName','Metric','AssignmentType','PurchaseDate','Quantity','IsIncomplete','UpdatedDate','UpdatedBy']
+```
+
+## Repository
+This script is maintained in the GitHub Repository found [here](https://github.com/SnowSoftware/ap-psws-GetSLMLicenses).  
+Please use GitHub issue tracker if you have any issues with the module. 
---
+++ scripts/ap-psws-GetSLMLicenses.ps1
@@ -0,0 +1,84 @@
+function get-pswsslmlicenses {
+    param($LicenseId, $filter)
+    
+    
+    #region debug setup
+    if ([Environment]::UserInteractive -eq $true -or $APDebugMode) {
+        if (-not (Get-Module IgapCore)) {
+            try {
+                #this part requires that the workflow engine is installed on the machine running this script
+                $wfeLocation = (Get-WmiObject win32_service | where-object { $_.Name -eq "Snow Automation Platform Workflow Engine" } | Select-Object PathName).PathName
+                $wfeDirectory = $wfeLocation.Substring(1, $wfeLocation.LastIndexOf('\') - 1) 
+                $apdirectory = $wfeDirectory.Substring(0, $wfeDirectory.LastIndexOf('\'))
+                $apdirectory
+                Import-Module "$apdirectory\CoreScripts\IgapCore.psm1"
+            }
+            catch {
+                #if the workflow engine is not installed on the machine running this script, update the path below
+                Import-Module 'C:\Program Files\Snow Software\Snow Automation Platform\CoreScripts\IgapCore.psm1'
+            }
+            
+        }
+        if (Get-Module IgapCore) {
+            if (Test-Path -Path Function:Write-Host) { remove-item -Path Function:Write-Host }
+            if (Test-Path -Path Function:Write-Verbose) { remove-item -Path Function:Write-Verbose }
+            if (Test-Path -Path Function:Write-Warning) { remove-item -Path Function:Write-Warning }
+            if (Test-Path -Path Function:Write-Error) { remove-item -Path Function:Write-Error }
+        }
+    }
+    #endregion
+    
+    #SLMHelper is available at https://github.com/SnowSoftware/slm-module-SLMHelper
+    if (-not (Get-Module SLMHelper)) {
+        Try {
+            Import-Module SLMHelper
+        }
+        catch {
+            Write-Error "Failed to import SLMHelper module."
+            return
+        }
+    }
+    
+    #region SLM Settings
+    $SLMApiAccountName = Get-APSetting "SLMApiAccountName"
+    $SLMUri = Get-APSetting "SLMUri"
+    $SLMCustomerId = Get-APSetting "SLMCustomerId"
+    $SLMUserAccount = Get-ServiceAccount -Name $SLMApiAccountName
+    
+    $securePassword = ConvertTo-SecureString $($SLMUserAccount.Password) -AsPlainText -Force
+    $SLMApiCredentials = New-Object Management.Automation.PSCredential ($($SLMUserAccount.AccountName), $securePassword)
+    #endregion
+    
+    $SLMApiEndpointConfiguration = New-SLMApiEndpointConfiguration -SLMUri $SLMUri -SLMCustomerId $SLMCustomerId -SLMApiCredentials $SLMApiCredentials -CleanupBody
+    
+    
+    if (-not [string]::IsNullOrEmpty($filter)) {
+        $SLMLicenses = Get-SLMLicenses -SLMApiEndpointConfiguration $SLMApiEndpointConfiguration -filter $filter
+        return $SLMLicenses
+    }
+    
+    if ([int]$LicenseId -gt 0) {
+        $SLMLicenses = Get-SLMLicenses -SLMApiEndpointConfiguration $SLMApiEndpointConfiguration -Id $LicenseId
+        return $SLMLicenses
+    }
+   
+    $SLMLicenses = Get-SLMLicenses -SLMApiEndpointConfiguration $SLMApiEndpointConfiguration
+    return $SLMLicenses
+    
+}
+    
+#Id              
+#ApplicationName 
+#ManufacturerName
+#Metric          
+#AssignmentType  
+#PurchaseDate    
+#Quantity        
+#IsIncomplete    
+#UpdatedDate     
+#UpdatedBy       
+    
+    
+#get-pswsslmlicenses
+#get-pswsslmlicenses -LicenseId 3
+#get-pswsslmlicenses -filter "ApplicationName eq 'IBM Spectrum Protect 7.1 for Mail'"
